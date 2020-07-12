6afd6d3645e1f0b2f17c43ebc96b9727b6e14e61
Freddy875 <fer875nandosm@gmail.com>
2020-07-11 19:30:13 -0500

    Estamos aprendiendo C# con nuestro primer Hola Mundo

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("HelloWorld");
+
+            Console.WriteLine("Hola Mundo");
+
+        }//fin static void Main
+
+    }//fin class Program
+
+}//fin namespace
